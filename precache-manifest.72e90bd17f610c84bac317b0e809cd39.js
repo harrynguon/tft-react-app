509f0e9df11c7629af2795b832b20557d292d77e
Harry Nguon <harrynguon@gmail.com>
2019-09-03 21:01:18 +1200

    Updates

---
+++ precache-manifest.72e90bd17f610c84bac317b0e809cd39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107d639c7be5071df89826c8f4eb1077",
+    "url": "/tft-react-app/index.html"
+  },
+  {
+    "revision": "ab75adc788ad92c07257",
+    "url": "/tft-react-app/static/css/main.2f78b3a0.chunk.css"
+  },
+  {
+    "revision": "979180f0fb88fe9d85ad",
+    "url": "/tft-react-app/static/js/2.76effcee.chunk.js"
+  },
+  {
+    "revision": "ab75adc788ad92c07257",
+    "url": "/tft-react-app/static/js/main.93183c6d.chunk.js"
+  },
+  {
+    "revision": "a07545e9adb398bc582f",
+    "url": "/tft-react-app/static/js/runtime~main.98dd4a49.js"
+  }
+]);
