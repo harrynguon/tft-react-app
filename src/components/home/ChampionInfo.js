fa54bd17fec9ddce602d35e11b445d1e8a45914b
Harry Nguon <harrynguon@gmail.com>
2019-09-06 20:45:13 +1200

    Cleaned up code

--- src/components/home/ChampionInfo.js
+++ src/components/home/ChampionInfo.js
@@ -3,15 +3,6 @@ import { connect } from 'react-redux';
 
 const ChampionInfo = (props) => {
     const champion = props.champion;
-    // ability
-    // class
-    // cost
-    // id
-    // items
-    // key
-    // name
-    // origin
-    // stats
     return (
         <div className="container center">
             <h4>Champion: <b>{ champion.name }</b></h4>
@@ -92,63 +83,6 @@ const ChampionInfo = (props) => {
                     </div>
                 </div>
             </div>
-
-            {/**  UNFORMATTED DATA
-
-            <div className="section">
-                <h5>About</h5>
-                <p>ID: { champion.id }</p>
-                <p>Origin: { champion.origin.join(', ') }</p>
-                <p>Class: { champion.class.join(', ') }</p>
-                <p>Cost: { champion.cost }</p>
-            </div>
-
-            <div className="divider" />
-
-            <div className="section">
-                <h5>Preferred Items</h5>
-                <ul className="collection">
-                    { champion.items.map(item => <li className="collection-item" key={champion.id+item} >{ item }</li>) }
-                </ul>
-            </div>
-
-            <div className="divider" />
-
-            <div className="section">
-                <h5>Stats</h5>
-                <h6>Offense</h6>
-                <p>Damage: { champion.stats.offense.damage }</p>
-                <p>Attack Speed: { champion.stats.offense.attackSpeed }</p>
-                <p>DPS: { champion.stats.offense.dps }</p>
-                <p>Range: { champion.stats.offense.range }</p>
-                <h6>Defense</h6>
-                <p>Health: { champion.stats.defense.health }</p>
-                <p>Armor: { champion.stats.defense.armor }</p>
-                <p>Magic Resist: { champion.stats.defense.magicResist }</p>
-            </div>
-
-            <div className="divider" />
-
-            <div className="section">
-                <h5>Ability</h5>
-                <p>Name: { champion.ability.name }</p>
-                Description: <blockquote>{ champion.ability.description }</blockquote>
-                <p>Type: { champion.ability.type }</p>
-                <p>Mana Cost: { champion.ability.manaCost }</p>
-                <p>Mana Start: { champion.ability.manaStart }</p>
-                Stats:
-                <ul className="collection">
-                    { champion.ability
-                                .stats
-                                .map(stat =>    <li className="collection-item" 
-                                                    key={champion.id + stat.type} 
-                                                    >
-                                                        Type: { stat.type } <br></br> 
-                                                        Value: { stat.value }
-                                                </li>) }
-                </ul>
-            </div>
-            */}
         </div>
     );
 }
--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -2,50 +2,16 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    // <footer className="page-footer grey darken-3">
-    //     <div className="container">
-    //         <div className="row">
-    //                 <div>
-    //                 <p className="grey-text text-lighten-4 left">All data is pulled from the Riot Games API. A valid internet connection is required.</p>
-    //                 </div>
-    //         </div>
-    //     </div>
-    //     <div className="footer-copyright grey darken-4">
-    //         <div className="container">
-    //         <p className="left">Copyright © 2019 Harry Nguon</p>
-    //         <p className="right">All data is pulled from the Riot Games API. A valid internet connection is required.</p>
-    //         </div>
-    //     </div>
-    // </footer>
-
-
     <footer className="grey">
         <div className="container section">
             <div className="center grey-text text-lighten-4">
                 All data is pulled from the Riot Games API. A valid internet connection is required.
-                
             </div>
         </div>
         <div className="section grey darken-1 no-padding">
         <div className="center grey-text text-lighten-2">Copyright © 2019 Harry Nguon</div>
         </div>
-        
     </footer>
-
-
-    // <footer className="page-footer grey no-padding">
-    //     {/** Footer requires two components in the framework, so colour both the same colour as only using one */}
-    //     <div className="footer-copyright grey">
-    //         <div className="container">
-    //                 <div className="col l6">
-    //                     <p className="left">All data is pulled from the Riot Games API. A valid internet connection is required.</p>
-    //                 </div>
-    //                 <div className="col l6">
-    //                     <p className="right">Copyright © 2019 Harry Nguon</p>
-    //                 </div> 
-    //         </div>
-    //     </div>
-    // </footer>
   );
 }
 
